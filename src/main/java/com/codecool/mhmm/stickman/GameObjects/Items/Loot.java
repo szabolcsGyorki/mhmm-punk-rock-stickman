134a83404923b70b4949d9a07ac80f9f67898f2c
Fábián János <fabian.janos93@gmail.com>
2018-09-26 14:42:41 +0200

    i think loot has been done

--- src/main/java/com/codecool/mhmm/stickman/GameObjects/Items/Loot.java
+++ src/main/java/com/codecool/mhmm/stickman/GameObjects/Items/Loot.java
@@ -1,10 +1,11 @@
 package com.codecool.mhmm.stickman.GameObjects.Items;
 
+import com.codecool.mhmm.stickman.DAO.DAOImpl.ItemsDAOImpl;
 import com.codecool.mhmm.stickman.DAO.ItemsDAO;
 import com.codecool.mhmm.stickman.GameObjects.GameObject;
 import com.codecool.mhmm.stickman.GameObjects.GameObjectType;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -12,12 +13,15 @@ import java.util.Random;
 @Entity
 public class Loot extends Item {
 
-    private List<Item> loot = new ArrayList<>();
-    private ItemsDao itemsList;
+    @ManyToMany
+    private ArrayList<Item> loot = new ArrayList<>();
+
+    @Transient
+    private ItemsDAOImpl itemsList;
+
     private int gold;
     private Random random = new Random();
 
-    public Loot();
     public Loot(int X, int Y, ItemsDAOImpl ItemsDAO) {
         super("Loot",0);
         this.itemsList = ItemsDAO;
@@ -28,19 +32,26 @@ public class Loot extends Item {
         add();
     }
 
+    public void setItemsList(ItemsDAOImpl itemsList) {
+        this.itemsList = itemsList;
+    }
+
+    public Loot() {
+    }
+
     public void add() {
         int numberOfLoots = random.nextInt(3) + 1;
         fillUpLoot(numberOfLoots);
     }
 
-    public void add(List<Item> items){
+    public void add(ArrayList<Item> items){
         loot = items;
     }
 
     private void fillUpLoot(int numberOfLoots) {
         for (int i = 0; i < numberOfLoots; i++) {
             Random random = new Random();
-            int itemNumber = random.nextInt(itemsList.getAllItems().size);
+            int itemNumber = random.nextInt(itemsList.getAllItems().size());
             GameObject item = itemsList.getItem(itemNumber);
             if (item instanceof Item) {
                 loot.add((Item) item);
