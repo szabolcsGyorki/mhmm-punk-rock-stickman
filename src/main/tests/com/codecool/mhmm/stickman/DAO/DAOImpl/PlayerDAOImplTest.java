237fe00b12441b0dece877e86fdcfa4e03984664
szabolcsGyorki <sz.gyorki@gmail.com>
2018-09-26 22:27:44 +0200

    Added constructor tests

--- src/main/tests/com/codecool/mhmm/stickman/DAO/DAOImpl/PlayerDAOImplTest.java
+++ src/main/tests/com/codecool/mhmm/stickman/DAO/DAOImpl/PlayerDAOImplTest.java
@@ -1,7 +1,21 @@
 package com.codecool.mhmm.stickman.DAO.DAOImpl;
 
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerDAOImplTest {
 
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("stickman");
+    private static EntityManager em = emf.createEntityManager();
+
+    @Test
+    void testInstanceCreated() {
+        PlayerDAOImpl dao = new PlayerDAOImpl(em);
+        assertNotNull(dao);
+    }
 }
